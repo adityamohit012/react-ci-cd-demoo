0aa81568368fb441e7e9d11c2ad60fee12d2cfea
adityamohit012 <adityamohitjangir0@gmail.com>
2025-11-01 10:45:24 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚀 React CI/CD Demo</h1>
+      <p>This app automatically builds and deploys using GitHub Actions!</p>
+    </div>
+  );
+}
+
+export default App;
